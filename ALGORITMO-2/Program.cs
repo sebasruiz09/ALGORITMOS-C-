8291934b3de45acce7c34556aaed80c8e7b5e6a6
sebasruiz09 <sebasruiz909@gmail.com>
2021-11-29 20:57:41 -0500

    algoritmos 1-2-3

---
+++ ALGORITMO-2/Program.cs
@@ -0,0 +1,23 @@
+﻿//hallar numeros pares del 1 al 100
+
+using System;
+
+namespace algoritmo_2
+{
+    class ejercicio
+    {
+        static void Main()
+        {
+            int i, impar = 0,;
+
+            for (i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write("{0}|", i);
+                    impar = impar + 1;
+                }
+            }
+        }
+    }
+}
---
+++ ALGORITMO-3/Program.cs
@@ -0,0 +1,16 @@
+﻿//  
+
+using System;
+
+namespace algoritmo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            if (i % 3 == 0 || i % 2 == 0)
+                Console.Write("{0}|", i);
+        }
+    }
+}
