9a176897474479de83703edc25f9302c22ce3144
sebasruiz09 <sebasruiz909@gmail.com>
2021-12-02 20:52:04 -0500

    algoritmos

--- ALGORITMO-15/Program.cs
+++ ALGORITMO-15/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//algoritmo Numero Perfecto
+using System;
 
 namespace algoritmo_15
 {
@@ -6,7 +7,26 @@ namespace algoritmo_15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0;
+            Console.Write("numero :");
+            x = Int32.Parse(Console.ReadLine());
+            int sum = 0;;
+            for (int i = 1; i < x; i++)
+            {
+                if ((x % i) == 0)
+                {
+                    sum=+1;
+                }
+            }
+                if (x == sum)
+                {  
+                    Console.Write("el numero es perfecto: " + Convert.ToString(x));
+                }
+                else
+                {
+                    Console.Write("el numero no es perfecto");
+                }
+
         }
     }
 }
--- ALGORITMO-16/Program.cs
+++ ALGORITMO-16/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//conversion de temperatura / C - K / C - F
+
+using System;
 
 namespace algoritmo_16
 {
@@ -6,7 +8,26 @@ namespace algoritmo_16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int celcius = 0, farenheit = 0, kelvin = 0;
+            string tipo = " ";
+            Console.Write("tipo de temperatura a convertir: ");
+            tipo = Convert.ToString(Console.ReadLine());
+            Console.Write("\n grados");
+            celcius = Int32.Parse(Console.ReadLine());
+
+            if(tipo == "fahrenheit")
+            {
+                farenheit = (celcius * 9/5) + 32;
+                Console.Write("grados fahrenheit: " + Convert.ToString(farenheit));
+            }
+            else
+            {
+                if(tipo == "kelvin")
+                {
+                    kelvin = celcius + 273; 
+                    Console.Write("kelvin: " + Convert.ToString(kelvin));
+                }
+            }
         }
     }
 }
